291c46614b67e069a6ef22a18618e84f07f8f9b9
Elian <elian.enria1@gmail.com>
2025-09-12 19:11:47 -0300

    closes #4
    Capa persistente

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ImcModule } from './module/imc/imc.module';
 import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
 
 
 @Module({
   imports: [ImcModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
--- src/module/imc/imc.controller.ts
+++ src/module/imc/imc.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, Get } from '@nestjs/common';
 import { ImcService } from './imc.service';
 import { CalcularImcDto } from './dto/calcular-imc-dto';
-
 
 @Controller('imc')
 export class ImcController {
   constructor(private readonly imcService: ImcService) {}
 
+  // POST /imc/calcular → calcula y guarda en Supabase
   @Post('calcular')
   calcular(@Body(ValidationPipe) data: CalcularImcDto) {
     return this.imcService.calcularImc(data);
+  }
+
+  // GET /imc/historial → devuelve todos los cálculos guardados
+  @Get('historial')
+  historial() {
+    return this.imcService.historial();
   }
 }
